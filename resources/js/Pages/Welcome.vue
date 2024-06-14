7026dcf969426a88ff05d5dde2d6245e722e0eae
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-06-14 10:11:02 +0700

    Add Footer

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,37 +1,45 @@
 <template>
-    <Head title="Welcome" />
-    <Navbar />
-    <section id="master" class="relative w-full h-full overflow-hidden">
-        <div class="m-0 p-0 w-full h-full flex relative">
-            <img
-                ref="masterImage"
-                src="../../../assets/img/monas.png"
-                alt="Master Image"
-                class="m-0 p-0 w-full lg:h-[500px] h-full object-cover zoom-effect"
-            />
-            <div
-                class="absolute top-0 left-0 w-full h-full bg-black opacity-50"
-            ></div>
+    <div>
+        <Head title="Beranda" />
+        <Navbar />
+        <section
+            id="master"
+            class="relative w-full h-full overflow-hidden pb-32"
+        >
+            <div class="m-0 p-0 w-full h-full flex relative">
+                <img
+                    ref="masterImage"
+                    src="../../../assets/img/monas.png"
+                    alt="Master Image"
+                    class="m-0 p-0 w-full lg:h-[500px] h-full object-cover zoom-effect"
+                />
+                <div
+                    class="absolute top-0 left-0 w-full h-full bg-black opacity-50"
+                ></div>
 
-            <div
-                class="absolute top-0 left-0 w-full h-full flex items-center justify-center"
-            >
-                <SearchDocument class="rounded-lg"></SearchDocument>
+                <div
+                    class="absolute top-0 left-0 w-full h-full flex items-center justify-center"
+                >
+                    <SearchDocument class="rounded-lg"></SearchDocument>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
 import { Head } from "@inertiajs/vue3"; // Pastikan ini adalah paket yang benar
 import Navbar from "@/Components/Navbar.vue";
 import SearchDocument from "@/Components/SearchDocument.vue";
+import Footer from "@/Components/Footer.vue";
 
 export default {
     components: {
         Head,
         Navbar,
         SearchDocument,
+        Footer,
     },
     mounted() {
         window.addEventListener("scroll", this.handleScroll);
